a0188ef2ed4b179eb0ac3481b6b2b68e6ce62ca8
JAEDEOKJO <lubiksss@gmail.com>
2021-08-12 18:32:32 +0900

    upgrade_mario

--- RL_Mario_test.py
+++ RL_Mario_test.py
@@ -1,3 +1,4 @@
+from env_transform import *
 import time
 import gym
 from collections import deque as dq
@@ -10,22 +11,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+# Mario의 state가 (4, 84, 84)sahpe, action은 2개이기 때문에 input 3, output 2인 CNN생성
 class Qnet(nn.Module):
     def __init__(self):
         super(Qnet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(55632, 64)
-        self.fc2 = nn.Linear(64, 2)
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(5184, 512)
+        self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
-        x = x.to(device)
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, 55632)
+        x = x.to(gpu)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 5184)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x.float()
@@ -34,30 +40,25 @@ class Qnet(nn.Module):
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, [['right'], ['right', 'A']])
 
-q = Qnet().to(device)
+q = Qnet().to(gpu)
 q.load_state_dict(torch.load('Mario_weight.pt'))
 
 # 주피터 노트북에서 실행 시 env.render()함수가 동작하지 않습니다.
 # 시각적으로 보는게 좋을거같아서 .py파일로 만들고 실행시켰습니다.
+env = SkipFrame(env, skip=4)
+env = GrayScaleObservation(env)
+env = ResizeObservation(env, shape=84)
 state = env.reset()
-state = Image.fromarray(state).convert('L')
-state = np.array(state, 'uint8')
-state = state.reshape((1,)+state.shape)
-state = state.copy()
-done = False
+print(state.shape)
+
 
 for i in range(1000+1):
 
     env.render()
     time.sleep(0.01)
-    action = q.forward(torch.from_numpy(
-        state.reshape((1,)+state.shape)).float()).argmax().item()
+    action = q.forward(state.unsqueeze(0).unsqueeze(0)).argmax().item()
 
     state, reward, done, info = env.step(action)
-    state = Image.fromarray(state).convert('L')
-    state = np.array(state, 'uint8')
-    state = state.reshape((1,)+state.shape)
-    state = state.copy()
 
     if done or i == 1000:
         print(i)
--- test.py
+++ test.py
@@ -1,32 +1,78 @@
-import time
+import random
+from collections import deque as dq
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import numpy as np
-import matplotlib.image as img
-from torchvision import transforms as T
-import matplotlib.pyplot as plt
-
+# Modules needed to transform Mario_env to fit ML Model
+from env_transform import *
+# Gym is an OpenAI toolkit for RL
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
 # NES Emulator for OpenAI Gym
 from nes_py.wrappers import JoypadSpace
-
 # Super Mario environment for OpenAI Gym
 import gym_super_mario_bros
-
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-# env = gym_super_mario_bros.make('SuperMarioBros-8-4-v0')
+# Initialize Super Mario environment
+env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0")
 # Limit the action-space to
 #   0. walk right
 #   1. jump right
-env = JoypadSpace(env, [['right'], ['right', 'A']])
+env = JoypadSpace(env, [["right"], ["right", "A"]])
 
-test = env.reset()
-# permute [H, W, C] array to [C, H, W] tensor
-test = np.transpose(test, (2, 0, 1))
-test = torch.tensor(test.copy(), dtype=torch.float)
-transform = T.Grayscale()
-test = transform(test)
+# Apply Wrappers to environment
+env = SkipFrame(env, skip=4)
+env = GrayScaleObservation(env)
+env = ResizeObservation(env, shape=84)
+# env = FrameStack(env, num_stack=4)
 
-plt.imshow(test.permute(1, 2, 0))
-plt.show()
+# NN를 학습시키기 위한 hyperparameter
+learning_rate = 0.0005
+batch_size = 32
+gamma = 0.98
+buffer_limit = 100000
+
+gpu = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 강화학습은 Training data set이라는게 따로 없다. Agent가 행동을 취하고 데이터셋을 쌓아나가야합니다.
+# 그 데이터셋을 쌓기 위한 버퍼
+
+
+class ReplayBuffer():
+    def __init__(self):
+        self.buffer = dq(maxlen=buffer_limit)
+
+    # 버퍼에는 (state, action ,reward, nstate, done) 값이 들어갑니다.
+    def put(self, transition):
+        self.buffer.append(transition)
+
+    # 샘플 함수를 만드는 이유는 버퍼에 쌓인 데이터셋에서 랜덤으로 학습을 시키기 위함입니다.
+    # 그냥 연속해서 쌓인 n개의 데이터셋을 그대로 사용하면 데이터간의 상관관계가 너무 크기 때문에 학슴이 잘 안됩니다.
+    def sample(self, n):
+        mini_batch = random.sample(self.buffer, n)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
+
+        for transition in mini_batch:
+            s, a, r, s_prime, done_mask = transition
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r])
+            s_prime_lst.append(s_prime)
+            done_mask_lst.append([done_mask])
+
+        return torch.tensor(s_lst, dtype=torch.float, device=gpu), torch.tensor(a_lst, device=gpu), \
+            torch.tensor(r_lst, dtype=torch.float, device=gpu), torch.tensor(s_prime_lst, dtype=torch.float, device=gpu), \
+            torch.tensor(done_mask_lst, dtype=torch.float, device=gpu)
+
+    def size(self):
+        return len(self.buffer)
+
+
+memory = ReplayBuffer()
+s = env.reset()
+a = 0
+s_prime, r, done, i = env.step(a)
+memory.put((s.unsqueeze(0), a, r/100.0, s_prime.unsqueeze(0), done))
+
+memory.sample(1)
